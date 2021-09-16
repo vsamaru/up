cb456633d16a6d0865a3dc6ed71885d7cefffa84
vsamaru <vsamaru@gmail.com>
2021-09-16 17:59:10 +0300

    first commit

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ console.log(params)
 })
 
 
-router.get('/t', async re => {
+router.get('/:z', async ({params}) => {
 
     // var e = params.id.slice(0, 1)
     // var c = params.id.slice(1)
-var t = "7-09-15"
+var t = params.z || "7-09-15"
     const image = await fetch(`https://8cc.netlify.app/https%3A%2F%2Ftelegra.ph%2F${t}`)
 
     const {
